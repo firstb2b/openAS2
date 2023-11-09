fe67672d6e6196368364aebe786577550a7ea51f
Tobias Leyland <tobias.leyland@firstb2b.com>
2023-11-09 15:29:41 +0000

    Modifying the xml at container runtime

--- start-container.sh
+++ start-container.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
+PARTNERSHIPS_FILE="/opt/openas2/config/partnerships.xml"
 if [ ! -e /opt/openas2/config/config.xml ]
     then
         echo "The config folder is empty, it will be populated by the template..."
         cp -a config_template/* config/
         echo "Done!"
 fi
+
+# Perform XML substitution
+sed "s/\${RECEIVING_PARTNERSHIP_NAME}/$RECEIVING_PARTNERSHIP_NAME/g" "$PARTNERSHIPS_FILE" > /tmp/tmpfile && cp -p /tmp/tmpfile "$PARTNERSHIPS_FILE"
+sed "s/\${SENDING_PARTNERSHIP_NAME}/$SENDING_PARTNERSHIP_NAME/g" "$PARTNERSHIPS_FILE" > /tmp/tmpfile && cp -p /tmp/tmpfile "$PARTNERSHIPS_FILE"
+sed "s/\${PARTNER_NAME}/$PARTNER_NAME/g" "$PARTNERSHIPS_FILE" > /tmp/tmpfile && cp -p /tmp/tmpfile "$PARTNERSHIPS_FILE"
+sed "s/\${PARTNER_AS2_ID}/$PARTNER_AS2_ID/g" "$PARTNERSHIPS_FILE" > /tmp/tmpfile && cp -p /tmp/tmpfile "$PARTNERSHIPS_FILE"
+
 $(dirname $0)/start-openas2.sh
